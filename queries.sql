77cd3ade1ba07a34920d382982432eaec4efb540
RatPeureux <leobleas@gmail.com>
2025-03-03 08:27:14 +0100

    feat: maj to min

--- queries.sql
+++ queries.sql
@@ -1,33 +1,32 @@
-SELECT country, COUNT(*) AS nombre_villes_par_pays
-FROM _ville
-GROUP BY country;
+select country, count(*) as nombre_villes_par_pays
+from _ville
+group by country;
 
-SELECT continent, COUNT(DISTINCT country) AS nombre_pays
-FROM _pays_continent
-GROUP BY continent;
+select continent, count(distinct country) as nombre_pays
+from _pays_continent
+group by continent;
 
-SELECT pc.continent, COUNT(v.id) AS nombre_villes_par_continent
-FROM _ville v
-JOIN _pays_continent pc ON v.country = pc.country
-GROUP BY pc.continent;
+select pc.continent, count(v.id) as nombre_villes_par_continent
+from _ville v
+join _pays_continent pc on v.country = pc.country
+group by pc.continent;
 
--- Nombre maximum de villes par pays
+-- nombre maximum de villes par pays
 
--- Nombre moyen de villes par continent
+-- nombre moyen de villes par continent
 
-SELECT continent, COUNT(country) AS max_pays_par_continent
-FROM _pays_continent
-GROUP BY continent
-ORDER BY max_pays DESC
-LIMIT 1;
+select continent, count(country) as max_pays_par_continent
+from _pays_continent
+group by continent
+order by max_pays desc
+limit 1;
 
--- Importer les pays / continents de puis CSV
+-- importer les pays / continents de puis csv
 copy t (x1, ... , x10)
 from '/path/to/my_file'
-with (format csv)
+with (format csv);
 
--- Importer les villes dans le monde depuis le CSV
+-- importer les villes dans le monde depuis le csv
 copy _ville (city, city_ascii, lat, lng, country, iso2, iso3, admin_name, capital, population, id)
 from '/files/worldcities.csv'
-with (format csv, HEADER)
-
+with (format csv, header);
