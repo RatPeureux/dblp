02261acc0d716eca91eaa5e4ff3831c52810dac8
zenpoxa <=>
2025-03-03 08:49:39 +0100

    dbconnection

--- html/index.php
+++ html/index.php
@@ -1,2 +1,10 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../includes/dbconnection.php';
+
+$stmt = $db->prepare("SELECT * FROM _ville");
+if ($stmt->execute()) {
+  $villes = $stmt->fetchAll();
+  print_r($villes);
+}
+
 
