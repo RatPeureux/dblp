edb3fb16beffd4ce78378cc99cdcdbc934da38de
zenpoxa <=>
2025-03-03 08:48:23 +0100

    dbconnection

--- html/index.php
+++ html/index.php
@@ -1,16 +1,2 @@
 <?php
-$csv_town_path = $_SERVER['DOCUMENT_ROOT'] . '/../files/worldcities.csv';
-
-$row = 1;
-
-if (($handle = fopen($csv_town_path, "r")) !== FALSE) {
-  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    $num = count($data);
-    echo "<p> $num fields in line $row: <br /></p>\n";
-    $row++;
-    for ($c=0; $c < $num; $c++) {
-      echo $data[$c] . "<br />\n";
-    }
-  }
-}
 
--- queries.sql
+++ queries.sql
@@ -22,12 +22,11 @@ ORDER BY max_pays DESC
 LIMIT 1;
 
 -- Importer les pays / continents de puis CSV
-copy t (x1, ... , x10)
-from '/path/to/my_file'
-with (format csv)
+copy _pays_continent (continent, country)
+from '/files/countriescontinents.csv'
+with (format csv, HEADER)
 
 -- Importer les villes dans le monde depuis le CSV
 copy _ville (city, city_ascii, lat, lng, country, iso2, iso3, admin_name, capital, population, id)
 from '/files/worldcities.csv'
 with (format csv, HEADER)
-
